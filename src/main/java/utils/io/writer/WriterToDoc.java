2e7ca870da6a3761700c80203ec90f0889c49a65
Nickirus <Nickirus@gmail.com>
2020-10-23 00:36:38 +0300

    change WriterToDoc

--- src/main/java/utils/io/writer/WriterToDoc.java
+++ src/main/java/utils/io/writer/WriterToDoc.java
@@ -3,10 +3,14 @@ package utils.io.writer;
 import model.QuestionWithEmbodiments;
 import model.Quiz;
 import org.apache.poi.xwpf.usermodel.*;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTPPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTSpacing;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.STLineSpacingRule;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.List;
 
 public class WriterToDoc implements QuizWriter{
@@ -18,24 +22,23 @@ public class WriterToDoc implements QuizWriter{
         for (Quiz quiz : quizList) {
             variant = fillVariantHeader(document, variant, quiz.getUuid());
 
-            XWPFParagraph paragraph = document.createParagraph();
-            XWPFRun textRun = paragraph.createRun();
-            textRun.setFontSize(12);
-            textRun.setFontFamily("Times New Roman");
-            textRun.addBreak();
-
             List<QuestionWithEmbodiments> questions = quiz.getQuestions();
             int questionNum = 1;
+            XWPFParagraph paragraph = document.createParagraph();
+            XWPFRun textRun = paragraph.createRun();
+            textRun.setFontSize(9);
+            textRun.setFontFamily("Times New Roman");
             for (QuestionWithEmbodiments question : questions) {
-                textRun.setText("Вопрос №" + questionNum++);
-                textRun.addBreak();
+
+                textRun.setText("№" + questionNum++ + ". ");
+
                 String questionText = question.getQuestionText();
                 textRun.setText(questionText);
                 textRun.addBreak();
                 List<String> answers = question.getAnswers();
                 int answerNum = 1;
                 for (String answer : answers) {
-                    textRun.setText(answerNum++ + ". " + answer);
+                    textRun.setText("       " + intToCharMap(answerNum++) + ". " + answer);
                     textRun.addBreak();
                 }
                 textRun.addBreak();
@@ -45,49 +48,70 @@ public class WriterToDoc implements QuizWriter{
         write(fileName, document);
     }
 
+    private String intToCharMap(int i) {
+        if (i==1) return "а";
+        if (i==2) return "б";
+        if (i==3) return "в";
+        if (i==4) return "г";
+        return "";
+    }
+
+    private void setSingleLineSpacing(XWPFParagraph para) {
+        para.setIndentFromLeft(-200);
+        CTPPr ppr = para.getCTP().getPPr();
+        if (ppr == null) ppr = para.getCTP().addNewPPr();
+        CTSpacing spacing = ppr.isSetSpacing()? ppr.getSpacing() : ppr.addNewSpacing();
+        spacing.setAfter(BigInteger.valueOf(0));
+        spacing.setBefore(BigInteger.valueOf(0));
+        spacing.setLineRule(STLineSpacingRule.AUTO);
+        spacing.setLine(BigInteger.valueOf(240));
+    }
+
     @Override
     public void writeQuizAnswers(List<Quiz> quizList, String fileName) {
         XWPFDocument document = new XWPFDocument();
         int variant = 1;
         for (Quiz quiz : quizList) {
-            variant = fillVariantHeader(document, variant, quiz.getUuid());
+            XWPFParagraph paragraphH = document.createParagraph();
+            paragraphH.setIndentFromLeft(-200);
+            setSingleLineSpacing(paragraphH);
+            paragraphH.setAlignment(ParagraphAlignment.LEFT);
+            XWPFRun headerRun = paragraphH.createRun();
+
+            headerRun.setFontSize(9);
+            headerRun.setBold(true);
+            headerRun.setFontFamily("Times New Roman");
+
+            headerRun.setText("Вариант " + variant++);
 
             XWPFParagraph paragraph = document.createParagraph();
+            setSingleLineSpacing(paragraph);
             XWPFRun textRun = paragraph.createRun();
-            textRun.setFontSize(12);
-            textRun.setFontFamily("Times New Roman");
-            textRun.addBreak();
+            textRun.setFontSize(8);
+            textRun.setFontFamily("Courier New");
+//            textRun.addBreak();
 
             List<QuestionWithEmbodiments> questions = quiz.getQuestions();
             int questionNum = 1;
             for (QuestionWithEmbodiments question : questions) {
-                textRun.setText("Вопрос №" + questionNum++ +" - " + question.getNumberOfTrueAnswer());
-                textRun.addBreak();
-                textRun.addBreak();
+                textRun.setText(questionNum++ +"-" + intToCharMap(question.getNumberOfTrueAnswer()) + "; ");
             }
-            textRun.addBreak(BreakType.PAGE);
         }
         write(fileName, document);
     }
 
     private int fillVariantHeader(XWPFDocument document, int variant, String uuid) {
         XWPFParagraph paragraph = document.createParagraph();
+//        setSingleLineSpacing(paragraph);
         paragraph.setAlignment(ParagraphAlignment.CENTER);
         XWPFRun headerRun = paragraph.createRun();
 
-        headerRun.setFontSize(14);
+        headerRun.setFontSize(10);
         headerRun.setBold(true);
         headerRun.setFontFamily("Times New Roman");
 
         headerRun.setText("Вариант " + variant++);
 
-        XWPFParagraph uuidParagraph = document.createParagraph();
-        uuidParagraph.setAlignment(ParagraphAlignment.CENTER);
-        XWPFRun uuidRun = uuidParagraph.createRun();
-        uuidRun.setFontSize(7);
-        uuidRun.setFontFamily("Courier New");
-        uuidRun.setText(uuid);
-        uuidRun.addBreak();
         return variant;
     }
 
